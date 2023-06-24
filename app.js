15792f7f0e897821cba0f2dca2289400a3385918
winter <jin-_-x@naver.com>
2023-06-24 17:48:45 +0900

    first step(2)

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-alert("hi");
+const a =5;
+const b =2;
+let myName = "cy";
+
+console.log(a+b);
+
+console.log(a*b);
+console.log(a/b);
+
+
+console.log("My name is " + myName);
+
+myName = "Jcy";
+
+console.log("My New Name is " + myName);
+
+
+const IamGosu = null;
+let something;
+
+console.log(IamGosu, something);
+
+
+//object 
+
+const player = {
+    name: crypto,
+    points : 100,
+    fat: true
+}
+
+
+console.log(player);
+player.lastName = "nico"
+console.log(player);
+
+player.points = player.points + 50;
+console.log(player.points);
+
+function sayHello(name) {
+console.log("Hello My name is " +name);
+}
+
+sayHello("nico");
+sayHello("cy");
+sayHello("backJun");
+
+
+
